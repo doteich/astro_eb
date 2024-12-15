31bc0b6267b951a00f253affa11d30bfab3ace7d
doteich <m.3ichhorn@googlemail.com>
2024-12-15 15:38:42 +0100

    updated icon

--- src/components/ui/icons/icons.ts
+++ src/components/ui/icons/icons.ts
@@ -256,6 +256,27 @@ export const Icons = {
     strokeLinecap: "round",
     strokeLinejoin: "round",
   },
+  layer: {
+    paths: [
+      {
+        d: "M12.412 14.572V10.29h1.428V16H1v-5.71h1.428v4.282z",
+       
+      },
+      {
+        d: "M3.857 13.145h7.137v-1.428H3.857zM10.254 0 9.108.852l4.26 5.727 1.146-.852zm-3.54 3.377 5.484 4.567.913-1.097L7.627 2.28l-.914 1.097zM4.922 6.55l6.47 3.013.603-1.294-6.47-3.013zm-.925 3.344 6.985 1.469.294-1.398-6.985-1.468z",
+       
+      },
+    ],
+    class:
+      "mt-2 h-6 w-6 flex-shrink-0 fill-neutral-700 hs-tab-active:fill-blue-400 dark:fill-neutral-300 dark:hs-tab-active:fill-blue-300 md:h-7 md:w-7",
+    width: 24,
+    height: 24,
+    viewBox: "0 0 25 24",
+    fill: "none",
+    strokeWidth: "1.0",
+    strokeLinecap: "round",
+    strokeLinejoin: "round",
+  },
   arrowUp: {
     paths: [
       {
